6051acd892c46dd5035c3a473c8629d297606631
Khaled Mahmoud <khaled.mahmoud@asgatech.com>
2022-01-25 14:49:40 +0200

    update readme.md file adding isBefore and is after methods create DateCharacteristcs struct to collect Date characteristics in adding test function of isBefore and is After

--- DateTransformer/DateFormater.swift
+++ DateTransformer/DateFormater.swift
@@ -13,51 +13,39 @@ class DateTransformer {
     ///
     /// - Parameters:
     /// - timeStamp: timeStamp value to be transformed to date
-    /// - format: format of the string representation.
-    /// - identifire: calendar identifre (.gregorian, .islamic, etc...).
-    /// - locale: to fix a custom locale.
+    /// - characteristcs: format of the string representation, calendar identifre,  custom locale.
     /// - Returns: string representation of the date.
-    static func dateFromTimeStamp(timeStamp: Double, format: String, identifier: Calendar.Identifier = .gregorian, locale: String) -> String {
+    static func dateFromTimeStamp(timeStamp: Double, characteristcs: DateCharacteristcs) -> String {
         let date = Date(timeIntervalSince1970: timeStamp)
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: locale)
-        formatter.dateFormat = format
-        formatter.calendar = Calendar(identifier: identifier)
+        let formatter = DateFormatter(characteristcs: characteristcs)
         return formatter.string(from: date)
     }
+    
     
     /// Convert date to a string using custom date format.
     ///
     /// - Parameters:
     /// - date: date to be transformed
-    /// - format: format of the string representation.
-    /// - identifire: calendar identifre (.gregorian, .islamic, etc...).
-    /// - locale: to fix a custom locale.
+    /// - characteristcs: format of the string representation, calendar identifre,  custom locale.
     /// - Returns: string representation of the date.
-    static func dateToString(date: Date, format: String, identifier: Calendar.Identifier = .gregorian, locale: String) -> String {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: locale)
-        formatter.dateFormat = format
-        formatter.calendar = Calendar(identifier: identifier)
+    static func dateToString(date: Date, characteristcs: DateCharacteristcs) -> String {
+        let formatter = DateFormatter(characteristcs: characteristcs)
         return formatter.string(from: date)
     }
+    
     
     /// Convert string to date using custom date format.
     ///
     /// - Parameters:
     /// - string: string to be transformed
-    /// - format: format of the string representation.
-    /// - locale: to fix a custom locale.
-    /// - identifre: calendar identifre (.gregorian, .islamic, etc...).
+    /// - characteristcs: format of the string representation, calendar identifre,  custom locale.
     /// - Returns: date representation of the string.
-    static func stringToDate(string: String, format: String, identifier: Calendar.Identifier = .gregorian, locale: String)-> Date? {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: locale)
-        formatter.dateFormat = format
-        formatter.calendar = Calendar(identifier: identifier)
+    static func stringToDate(string: String, characteristcs: DateCharacteristcs)-> Date? {
+        let formatter = DateFormatter(characteristcs: characteristcs)
         let date = formatter.date(from: string)
         return date
     }
+    
     
     /// Convert string with custom format To string using custom date format.
     ///
@@ -67,30 +55,115 @@ class DateTransformer {
     /// - locale: to fix a custom locale.
     /// - identifre: calendar identifre (.gregorian, .islamic, etc...).
     /// - Returns: string representation of the date.
-    static func stringToString(string: String, format: String, outputFormat: String, identifier: Calendar.Identifier = .gregorian, locale: String)-> String {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: locale)
-        formatter.dateFormat = format
-        formatter.calendar = Calendar(identifier: identifier)
+    static func stringToString(string: String, inputCharacteristcs: DateCharacteristcs, outputCharacteristcs: DateCharacteristcs)-> String {
+        var formatter = DateFormatter(characteristcs: inputCharacteristcs)
         guard let date = formatter.date(from: string) else {
             return "date string or format is not correct."
         }
-        formatter.dateFormat = outputFormat
+        formatter = DateFormatter(characteristcs: outputCharacteristcs)
         return formatter.string(from: date)
+    }
+    
+    
+    /// check if date is bfore another date
+    ///
+    /// - Parameters:
+    /// - firstDate: the first date object
+    /// - secondDate: second date object
+    /// - orEqual: a flage if need to check equality too.
+    /// - Returns: bool represntin the check result.
+    static func isBefore(firstDate: Date, secondDate: Date, orEqual: Bool = false) -> Bool {
+        if orEqual {
+            return firstDate <= secondDate
+        }
+        return firstDate < secondDate
+    }
+    
+    
+    /// check if date is bfore another date
+    ///
+    /// - Parameters:
+    /// - firstDate: the string of first date object
+    /// - firstDateCharacteristcs: characteristcs of string date.
+    /// - secondDate: the string of second date object
+    /// - secondDateCharacteristcs: characteristcs of string date.
+    /// - orEqual: a flage if need to check equality too.
+    /// - Returns: bool represntin the check result, return nil if firstDate or secondDate in wrong formate.
+    static func isBefore(firstDate: String, firstDateCharacteristcs: DateCharacteristcs,  secondDate: String, secondDateCharacteristcs: DateCharacteristcs, orEqual: Bool = false) -> Bool? {
+        guard let date1 = stringToDate(string: firstDate, characteristcs: firstDateCharacteristcs) else { return nil }
+        
+        guard let date2 = stringToDate(string: secondDate, characteristcs: secondDateCharacteristcs) else { return nil }
+        
+        if orEqual {
+            return date1 <= date2
+        }
+        return date1 < date2
+    }
+    
+    /// check if date is after another date
+    ///
+    /// - Parameters:
+    /// - firstDate: the first date object
+    /// - secondDate: second date object
+    /// - orEqual: a flage if need to check equality too.
+    /// - Returns: bool represntin the check result.
+    static func isAfter(firstDate: Date, secondDate: Date, orEqual: Bool = false) -> Bool {
+        if orEqual {
+            return firstDate >= secondDate
+        }
+        return firstDate > secondDate
+    }
+    
+    
+    /// check if date is after another date
+    ///
+    /// - Parameters:
+    /// - firstDate: the string of first date object
+    /// - firstDateCharacteristcs: characteristcs of string date.
+    /// - secondDate: the string of second date object
+    /// - secondDateCharacteristcs: characteristcs of string date.
+    /// - orEqual: a flage if need to check equality too.
+    /// - Returns: bool represntin the check result, return nil if firstDate or secondDate in wrong formate.
+    static func isAfter(firstDate: String, firstDateCharacteristcs: DateCharacteristcs, secondDate: String, secondDateCharacteristcs: DateCharacteristcs, orEqual: Bool = false) -> Bool? {
+        guard let date1 = stringToDate(string: firstDate, characteristcs: firstDateCharacteristcs) else { return nil }
+        
+        guard let date2 = stringToDate(string: secondDate, characteristcs: secondDateCharacteristcs) else { return nil }
+        
+        if orEqual {
+            return date1 >= date2
+        }
+        return date1 > date2
     }
 }
 
+extension DateFormatter {
+    convenience init(characteristcs: DateCharacteristcs) {
+        self.init()
+        locale = Locale(identifier: characteristcs.locale)
+        dateFormat = characteristcs.format
+        calendar = Calendar(identifier: characteristcs.identifier)
+    }
+}
+
+struct DateCharacteristcs {
+    var format: String
+    var locale: String = "en"
+    var identifier: Calendar.Identifier = .gregorian
+}
+
 enum DateFormats: String {
-    case year = "yyyy"
-    case month = "MM"
-    case day = "dd"
-    case hour = "h"
-    case minute = "m"
-    case day_MonthName = "dd MMM"
-    case monthName_Year = "MMM YYYY"
-    case day_month_year = "dd-MM-yyyy"
-    case time = "hh:mm a"
-    case dateTime = "dd-MM-yyyy hh:mm a"
-    case dateWithMonthName = "d MMM yyyy"
-    case isoFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    case YYYY = "yyyy" // ex: 2022
+    case MM = "MM"  // ex: 12
+    case DD = "dd"  // ex: 31
+    case H = "h"  // ex: 7
+    case M = "m"  // ex: 58
+    case DD_MMM = "dd MMM"  // ex: 13 Jan
+    case MMM_YYYY = "MMM YYYY"  // ex: Feb 2022
+    case DD_MM_YYYY = "dd-MM-yyyy"  // ex: 22-10-2022
+    case YYYY_MM_DD = "yyyy-MM-dd"  // ex: 2022-03-23
+    case HH_MM_A = "hh:mm a"  // ex: 07:30 PM
+    case DD_MM_YYYY_HH_MM_A = "dd-MM-yyyy hh:mm a"  // ex: 02-02-2022 04:30 PM
+    case D_MMM_YYYY = "d MMM yyyy"  // ex: 15 May 2021
+    case YYYY_MM_DD_T_HH_MM_SS = "yyyy-MM-dd'T'HH:mm:ss"  //ex: 2001-07-04T12:08:56
+    case YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss"  // ex: 2020-10-01 04:30:22
 }
--- README.md
+++ README.md
@@ -1,1 +1,98 @@
 # DateTransformer
+     this module is used to convert (Date -> String) or (String -> Date) with specific formate
+     also convert (hijri -> gregorian) or (gregorian -> hijri) 
+     and extract any part of date you need.
+
+## Installation
+
+DateTransformer is available through [CocoaPods](http://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod "DateTransformer"
+```
+
+# Usage
+
+```swift
+let today = Date()
+
+// convert date object to string of "day-month-year" format > ex: "25-01-2022"
+DateTransformer.dateToString(date: today,
+                             characteristcs: DateCharacteristcs(
+                             format: DateFormats.DD_MM_YYYY.rawValue)
+
+// convert date object to string of time format > ex: "10:34 AM"
+DateTransformer.dateToString(date: today,
+                             characteristcs: DateCharacteristcs(
+                             format: DateFormats.HH_MM_A.rawValue)
+
+// convert date object to string of date-time format > ex: "18-02-2022 07:00 PM"
+DateTransformer.dateToString(date: today,
+                             characteristcs: DateCharacteristcs(
+                             format: DateFormats.DD_MM_YYYY_HH_MM_A.rawValue)
+
+// convert date object to string of Hijri date format > ex: "١٧ رجب ١٤٤٣"
+DateTransformer.dateToString(date: today,
+                             characteristcs: DateCharacteristcs(
+                             format: DateFormats.D_MMM_YYYY.rawValue,
+                             locale: "Ar",
+                             identifier: .islamic))
+
+// convert string of "day-month-year" format ex: "18-02-2022" > Date object. 
+DateTransformer.stringToDate(string: "18-02-2022",
+                             characteristcs: DateCharacteristcs(
+                             format: DateFormats.DD_MM_YYYY.rawValue)
+                             
+// convert string of "year-month-day" format ex: "2022-01-25" > string of "day-month-year" format ex: "25-01-2022".
+DateTransformer.stringToString(string: "2022-01-25", 
+                               inputCharacteristcs: DateCharacteristcs(
+                               format: DateFormats.YYYY_MM_DD.rawValue), 
+                               outputCharacteristcs: DateCharacteristcs(
+                               format: DateFormats.DD_MM_YYYY.rawValue))
+
+// convert string of hijri calendar ex: "1443-06-17" > string of "day-month-year" format gregorian calendar "18-02-2022". 
+DateTransformer.stringToString(string: "1443-06-17", 
+                               inputCharacteristcs: DateCharacteristcs(
+                               format: DateFormats.YYYY_MM_DD.rawValue, 
+                               locale: "en", identifier: .islamic), 
+                               outputCharacteristcs: DateCharacteristcs(
+                               format: DateFormats.DD_MM_YYYY.rawValue,
+                               locale: "en", identifier: .gregorian))
+                               
+```      
+
+### Comparing dates
+
+```swift
+let date1 = Date() // ex: "25-01-2022"
+let date2 = Calendar.current.date(byAdding: .month, value: 1, to: date1) // ex: "25-02-2022"
+
+//check if date is before another date
+if DateTransformer.isBefore(firstDate: date1, secondDate: date2) {
+    //Do some thing
+}
+
+//check if date is before or equal another date
+if DateTransformer.isBefore(firstDate: date1, secondDate: date2, orEqual: true) {
+    //Do some thing
+}
+
+//check if string of date is before another string of date
+if DateTransformer.isBefore(firstDate: "01-01-2014",
+                            firstDateCharacteristcs: DateCharacteristcs(
+                            format: DateFormats.DD_MM_YYYY.rawValue), 
+                            secondDate: "01-01-2028", 
+                            secondDateCharacteristcs: DateCharacteristcs(
+                            format: DateFormats.DD_MM_YYYY.rawValue)) {
+    //Do some thing
+}
+
+also,
+
+//check if date is After another date
+if DateTransformer.isAfter(firstDate: date2, secondDate: date1) {
+    //Do some thing
+}
+
+```
